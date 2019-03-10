32ac0b00772e818932e8cd0d9c009f8204a17309
Fatih Baltacı <baltacifatih14@gmail.com>
2019-03-10 18:13:28 +0300

    Add ssh connection dialog

---
+++ src/redial/dialog.py
@@ -0,0 +1,39 @@
+import urwid
+
+from utils import append_to_config
+
+
+def question(content):
+    return urwid.Pile([urwid.Edit(('I say', content + " "))])
+
+
+class ConversationListBox(urwid.ListBox):
+    def __init__(self, questions):
+        self.answers = []
+        body = urwid.SimpleFocusListWalker([question(questions[0])])
+        super(ConversationListBox, self).__init__(body)
+
+    def keypress(self, size, key):
+        key = super(ConversationListBox, self).keypress(size, key)
+        if key != 'enter':
+            return key
+        answer = self.focus[0].edit_text
+        if not answer:
+            raise urwid.ExitMainLoop()
+        self.answers.append(answer)
+        pos = self.focus_position
+        # add a new question
+        if len(questions) > pos+1:
+            self.body.insert(pos + 1, question(questions[pos+1]))
+            self.focus_position = pos + 1
+        else:
+            raise urwid.ExitMainLoop()
+        
+
+palette = [('I say', 'default,bold', 'default'), ]
+questions = ["Host: ", "hostname(ip): ", "user: ", "port: "]
+conversation = ConversationListBox(questions)
+urwid.MainLoop(conversation, palette).run()
+
+
+append_to_config(conversation.answers)
