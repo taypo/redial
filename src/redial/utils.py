c5b5a6ae293856004784973fa0039a4086590846
Fatih Baltacı <baltacifatih14@gmail.com>
2019-03-10 18:14:12 +0300

    refactoring - auto create .ssh/config file

--- src/redial/utils.py
+++ src/redial/utils.py
@@ -2,37 +2,39 @@ from os.path import expanduser
 
 from .hostinfo import *
 
+import os
 
 def read_ssh_config():
     home = expanduser("~")
-    f = open(home + "/.ssh/config")
     hosts = []
+    directory = home + "/.ssh/"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    if os.path.isfile(directory + "config"):
+        with open(directory + "config", "r") as file:
+            for f_line in file:
+                line = f_line.strip()
+                if line.startswith('#'):
+                    continue
+                kv = line.split(' ')
+                h = HostInfo()
+                if kv[0].lower() == "host":
+                    h.full_name = kv[1]
+                    split = kv[1].split('/')
+                    h.name = split[len(split) - 1]
+                    hosts.append(h)
+                
+    else:
+        file = open(directory + "config", "w")
+        file.close()
 
-    for f_line in f:
-        line = f_line.strip()
-        if line.startswith('#'):
-            continue
-        kv = line.split(' ')
-        h = HostInfo()
-        if kv[0].lower() == "host":
-            h.full_name = kv[1]
-            split = kv[1].split('/')
-            h.name = split[len(split) - 1]
-        elif kv[0].lower() == "hostname":
-            h.ip = kv[1]
-        hosts.append(h)
-
-    # root = Folder()
-    #
-    # for h in hosts:
-    #     s = h.full_name.split('/')
-    #     if(len(s) == 1):
-    #         root.children.append(h)
-    #     else:
-    #
-    #
-    # for h in hosts:
-    #     print h.full_name + ":--------:" + h.name
-
-    f.close()
     return hosts
+
+
+def append_to_config(answers):
+    content = "\nHost " + answers[0] + "\n" + "hostname " + answers[1] + "\n" + "user " + answers[2] + "\n" + "port " + answers[3] + "\n\n"
+              
+    home = expanduser("~")
+    path = home + "/.ssh/config"
+    with open(path, "a+") as file:
+        file.write(content)
